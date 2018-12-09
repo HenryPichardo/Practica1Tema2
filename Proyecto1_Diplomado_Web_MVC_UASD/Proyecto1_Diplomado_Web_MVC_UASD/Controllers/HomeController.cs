6310c71f8a381d1d9faa29f81c78f11d71407c52
Henry Pichardo <henryfpichardog@hotmail.com>
2018-12-08 20:54:47 -0400

    Initial commit

---
+++ Proyecto1_Diplomado_Web_MVC_UASD/Proyecto1_Diplomado_Web_MVC_UASD/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1_Diplomado_Web_MVC_UASD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "<html><body>" +
+                "<h1>Universidad Autonoma de Santo Domingo (UASD)</h1>"+
+                "<p>Diplomado Desarrollo Web C#, MVC </p>"+
+                "</html></body>";
+        }
+        public string DiplomadoWeb()
+        {
+            return "<html><body>" +
+                "<h1>Estudiantes : </h1>" +
+                "<p>Henry Fernando Pichardo Gonzalez <br>" +
+                "guacanagarix Brgos<br>"+
+                "Yorki Montero Sanchez</p>"+
+                "</html></body>";
+        }
+    }
+}
